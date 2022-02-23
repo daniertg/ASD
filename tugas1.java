4701d9f7479c5629b3dc92ab7563c23705c325fc
daniertg <87335182+daniertg@users.noreply.github.com>
2022-02-23 18:48:56 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,14 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        int ani =4,budi = 15, bina = 6, cita = 11;
+        double total;
+        System.out.println("Harga per Customer : ");
+        System.out.println("Ani     Rp " +(4*4500));
+        System.out.println("Budi    Rp " +(15*4500*0.05));
+        System.out.println("Bina    Rp " +(6*4500));
+        System.out.println("Cita    Rp " +(11*4500*0.05));
+        
+        total = ((4*4500)+(15*4500*0.05)+(6*4500)+(11*4500*0.05));
+        System.out.println("Total Pendapatan Smile Laundry Per-Hari Adalah Rp. " +total);
+    }
+}

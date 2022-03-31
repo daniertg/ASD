88b1f2280693a4cf5bf19caa1ae6679d80ec3a58
daniertg <87335182+daniertg@users.noreply.github.com>
2022-03-31 08:42:49 +0700

    Update Main.java

--- Pertemuan6/Source code/Main.java
+++ Pertemuan6/Source code/Main.java
@@ -1,11 +1,11 @@
-public class Main {
+public class TiketMain {
     public static void main(String[] args) {
         TiketService ts = new TiketService();
-        Tiket t1 = new Tiket("Garuda" , 7000 , "Jakarta" , "Bali");
-        Tiket t2 = new Tiket("Shukoi" , 4000 , "Kediri" , "Bali");
-        Tiket t3 = new Tiket("Pesawat tempur" , 5000 , "Blitar" , "Bali");
-        Tiket t4 = new Tiket("Andromeda" , 15000 , "Surabaya" , "Bali");
-        Tiket t5 = new Tiket("Hydra" , 10000 , "Malang" , "Bali");
+        Tiket t1 = new Tiket("Garuda" , 10000 , "Jakarta" , "Bali");
+        Tiket t2 = new Tiket("Lion Air" , 15000 , "malang" , "Bali");
+        Tiket t3 = new Tiket("Citilink" , 5000 , "Aceh" , "Bali");
+        Tiket t4 = new Tiket("Air asia" , 15000 , "Surabaya" , "Jakarta");
+        Tiket t5 = new Tiket("jet pribadi" , 1000000 , "Medan" , "St.petersburg");
 
         ts.tambah(t1);
         ts.tambah(t2);
@@ -15,7 +15,7 @@ public class Main {
 
         System.out.println("\nTiket sebelum disorting : \n");
         ts.tampilAll();
-        
+
         System.out.println("\nTiket setelah disorting dengan bubblesort\n");
         ts.bubbleSort();
         ts.tampilAll();

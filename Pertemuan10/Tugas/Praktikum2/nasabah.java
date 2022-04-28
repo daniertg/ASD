1218c621cc9e09e929a9d1bdf667ac8e30643d96
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-04-29 01:27:11 +0700

    Create nasabah.java

---
+++ Pertemuan10/Tugas/Praktikum2/nasabah.java
@@ -0,0 +1,20 @@
+public class nasabah {
+    String norek;
+    String nama;
+    String alamat;
+    int umur;
+    double saldo;
+
+    nasabah(String norek, String nama, String alamat, int umur, double saldo){
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }
+
+    nasabah(){
+
+    }
+
+}

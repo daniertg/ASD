f7d96b9d3dc07160ca5e0b21566609e8d920dad8
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-06-09 23:42:06 +0700

    Create BinaryTreeArray.java

---
+++ Pertemuan14/Source code/BinaryTreeArray.java
@@ -0,0 +1,42 @@
+public class BinaryTreeArray {
+    int[] data;
+    int idxLast;
+    
+    public BinaryTreeArray(){
+        data = new int[10];
+    }
+
+    void add(int key){
+        idxLast++;
+        data[idxLast]= key;
+    }
+
+    void populateData(int data[], int idxLast){
+        this.data = data;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder(int idxStart){
+        if(idxStart <= idxLast){
+            traverseInOrder(2*idxStart+1);
+            System.out.println(data[idxStart] + " ");
+            traverseInOrder(2*idxStart+2);
+        }
+    }
+
+    void traversePreOrder(int idxStart){
+        if(idxStart <= idxLast){
+            System.out.println(data[idxStart] + " ");
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
+        }
+    }
+
+    void traversePostOrder(int idxStart){
+        if(idxStart <= idxLast){
+            traversePostOrder(2 * idxStart + 1);
+            traversePostOrder(2 * idxStart + 2);
+            System.out.println(data[idxStart] + " ");
+        }
+    }
+}

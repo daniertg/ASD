bf18d76b65e59378ff9a1e56405e3b995e3e07c6
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-06-09 23:41:15 +0700

    Create Node.java

---
+++ Pertemuan14/Source code/Node.java
@@ -0,0 +1,18 @@
+public class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(){
+
+    }
+    public Node(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+
+    boolean isLeaf(){
+        return left == null && right == null;
+    }
+}

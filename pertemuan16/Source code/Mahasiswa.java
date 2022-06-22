c14b264b663fb894c844686f0f29851ef72d9665
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-06-22 21:54:59 +0700

    Create Mahasiswa.java

---
+++ pertemuan16/Source code/Mahasiswa.java
@@ -0,0 +1,18 @@
+public class Mahasiswa {
+    String nim, nama, notelp;
+
+    public Mahasiswa(){
+
+    }
+
+    public Mahasiswa(String nim, String nama, String notelp){
+        this.nim=nim;
+        this.nama=nama;
+        this.notelp=notelp;
+    }
+
+    
+    public String toString(){
+        return "Mahasiswa{" + "nim=" + nim + ", nama=" + nama + ", notelp=" + notelp + '}';
+    }
+}
